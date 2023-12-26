aab3319f709ab1ef8ec2e8e519f533b606bb8ead
bob16795 <prestosilver.ptp@gmail.com>
2023-12-26 08:30:41 -0500

    update doom config

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" default))
+ '(global-wakatime-mode t)
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages '(elcord))
+ '(wakatime-api-key "waka_b75f70dc-e752-45d8-944c-b7c976465ce4")
+ '(wakatime-cli-path "wakatime-cli")
+ '(warning-suppress-log-types '(((flycheck syntax-checker)) (defvaralias)))
+ '(warning-suppress-types '(((flycheck syntax-checker)) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:height 2.5)))))
